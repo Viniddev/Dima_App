79f2c1813a0cbbdbf3bb54bd4d068dd8dfe30511
Vinicius Dias <diasvinicius95@outlook.com>
2024-11-29 09:56:04 -0300

    commit inicial

---
+++ Dima/Dima.Core/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Models
+{
+    public class Category : EntidadeBase
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+    }
+}
---
+++ Dima/Dima.Core/Models/EntidadeBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Models
+{
+    public class EntidadeBase
+    {
+        public long Id { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime DateUpdate { get; set; }
+        public bool Active { get; set; }
+    }
+}
